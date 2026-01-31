494bd8080667d0c1f267d3e5304032653da50257
KillLaAqua <KillLaAqua@gmail.com>
2026-01-31 14:14:30 -0600

    Lang and Textures

--- kubejs/startup_scripts/common/objects/blocks/fusion.js
+++ kubejs/startup_scripts/common/objects/blocks/fusion.js
@@ -6,11 +6,12 @@ StartupEvents.registry('block', event => {
             .tier(i+1)    
             .hardness(5)
             .resistance(10)
+            .transparent(true)
+            .defaultTranslucent() 
             .soundType('metal')
-            .tagBlock('mineable/pickaxe')
             .tagBlock('minecraft:needs_iron_tool')
-            .requiresTool(false)
-            .texture(`kubejs:item/progression/neutron_reflector/${reflector}`);
+            .requiresTool(true)
+            .texture(`kubejs:block/fusion/${reflector}_reflector/casing`);
     });   
 
 });
